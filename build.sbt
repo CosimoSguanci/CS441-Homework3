b2622c6d262a44bc59715673b1dffdfa74c8bf6c
CosimoSguanci <cosimo.sguanci@mail.polimi.it>
2021-10-31 00:24:23 -0500

    Updated Log generator to upload file to S3

--- build.sbt
+++ build.sbt
@@ -10,6 +10,7 @@ val typesafeConfigVersion = "1.4.1"
 val apacheCommonIOVersion = "2.11.0"
 val scalacticVersion = "3.2.9"
 val generexVersion = "1.0.2"
+val awsJavaSdkS3Version = "1.12.99"
 
 resolvers += Resolver.jcenterRepo
 
@@ -23,5 +24,6 @@ libraryDependencies ++= Seq(
   "org.scalatest" %% "scalatest" % scalacticVersion % Test,
   "org.scalatest" %% "scalatest-featurespec" % scalacticVersion % Test,
   "com.typesafe" % "config" % typesafeConfigVersion,
-  "com.github.mifmif" % "generex" % generexVersion
+  "com.github.mifmif" % "generex" % generexVersion,
+  "com.amazonaws" % "aws-java-sdk-s3" % awsJavaSdkS3Version
 )
--- src/main/scala/GenerateLogData.scala
+++ src/main/scala/GenerateLogData.scala
@@ -8,13 +8,18 @@
  *
  */
 import Generation.{LogMsgSimulator, RandomStringGenerator}
-import HelperUtils.{CreateLogger, Parameters}
+import HelperUtils.{CreateLogger, ObtainConfigReference, Parameters}
+import com.amazonaws.auth.{AWSStaticCredentialsProvider, BasicAWSCredentials}
+import com.amazonaws.regions.Regions
+import com.amazonaws.services.s3.model.{ObjectMetadata, PutObjectRequest}
 
 import collection.JavaConverters.*
 import scala.concurrent.{Await, Future, duration}
 import concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration.Duration
+import scala.io.Source
 import scala.util.{Failure, Success, Try}
+import com.amazonaws.services.s3.{AmazonS3, AmazonS3ClientBuilder}
 
 object GenerateLogData:
   val logger = CreateLogger(classOf[GenerateLogData.type])
@@ -30,11 +35,38 @@ object GenerateLogData:
   val INITSTRING = "Starting the string generation"
   val init = unit(INITSTRING)
 
+  val config = ObtainConfigReference("randomLogGenerator") match {
+    case Some(value) => value
+    case None => throw new RuntimeException("Cannot obtain a reference to the config data.")
+  }
+
+  val awsCredentials: BasicAWSCredentials = new BasicAWSCredentials(config.getString("randomLogGenerator.awsAccessKey"), config.getString("randomLogGenerator.awsSecretKey"))
+  val s3Client: AmazonS3 = AmazonS3ClientBuilder.standard().withCredentials(new AWSStaticCredentialsProvider(awsCredentials)).withRegion(Regions.US_EAST_2).build()
+  val bucketName = config.getString("randomLogGenerator.s3BucketName")
+
   val logFuture = Future {
     LogMsgSimulator(init(RandomStringGenerator((Parameters.minStringLength, Parameters.maxStringLength), Parameters.randomSeed)), Parameters.maxCount)
   }
+
   Try(Await.result(logFuture, Parameters.runDurationInMinutes)) match {
-    case Success(value) => logger.info(s"Log data generation has completed after generating ${Parameters.maxCount} records.")
+    case Success(value) => {
+      logger.info(s"Log data generation has completed after generating ${Parameters.maxCount} records.")
+
+      // Now I upload the log file to S3 and I delete the local file to avoid wasting resources
+
+      val dirName = "log"
+      val dir = new java.io.File(dirName)
+      val fileName = if(dir.exists && dir.isDirectory) dir.listFiles.filter(_.isFile).toList(0).getName else null
+      val filePath = s"$dirName/$fileName"
+      val file = new java.io.File(filePath)
+
+      val request: PutObjectRequest = new PutObjectRequest(bucketName, fileName, file)
+      val metadata: ObjectMetadata = new ObjectMetadata()
+      metadata.setContentType("plain/text")
+      request.setMetadata(metadata)
+      s3Client.putObject(request)
+      file.delete()
+    }
     case Failure(exception) => logger.info(s"Log data generation has completed within the allocated time, ${Parameters.runDurationInMinutes}")
   }
 
