d215b644eae0d04ba08e4566d018b293a00bbfa3
CosimoSguanci <cosimo.sguanci@mail.polimi.it>
2021-10-31 21:55:30 -0500

    Added LogFileGenerator

---
+++ LogFileGenerator/src/main/scala/GenerateLogData.scala
@@ -0,0 +1,74 @@
+/*
+ *
+ *  Copyright (c) 2021. Mark Grechanik and Lone Star Consulting, Inc. All rights reserved.
+ *
+ *   Unless required by applicable law or agreed to in writing, software distributed under
+ *   the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ *   either express or implied.  See the License for the specific language governing permissions and limitations under the License.
+ *
+ */
+import Generation.{LogMsgSimulator, RandomStringGenerator}
+import HelperUtils.{CreateLogger, ObtainConfigReference, Parameters}
+import com.amazonaws.auth.{AWSStaticCredentialsProvider, BasicAWSCredentials}
+import com.amazonaws.regions.Regions
+import com.amazonaws.services.s3.model.{GetObjectRequest, ObjectMetadata, PutObjectRequest, S3Object}
+
+import collection.JavaConverters.*
+import scala.concurrent.{Await, Future, duration}
+import concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration.Duration
+import scala.io.Source
+import scala.util.{Failure, Success, Try}
+import com.amazonaws.services.s3.{AmazonS3, AmazonS3ClientBuilder}
+
+object GenerateLogData:
+  val logger = CreateLogger(classOf[GenerateLogData.type])
+
+//this is the main starting point for the log generator
+@main def runLogGenerator =
+  import Generation.RSGStateMachine.*
+  import Generation.*
+  import HelperUtils.Parameters.*
+  import GenerateLogData.*
+
+  logger.info("Log data generator started...")
+  val INITSTRING = "Starting the string generation"
+  val init = unit(INITSTRING)
+
+  val config = ObtainConfigReference("randomLogGenerator") match {
+    case Some(value) => value
+    case None => throw new RuntimeException("Cannot obtain a reference to the config data.")
+  }
+
+  val awsCredentials: BasicAWSCredentials = new BasicAWSCredentials(config.getString("randomLogGenerator.awsAccessKey"), config.getString("randomLogGenerator.awsSecretKey"))
+  val s3Client: AmazonS3 = AmazonS3ClientBuilder.standard().withCredentials(new AWSStaticCredentialsProvider(awsCredentials)).withRegion(Regions.US_EAST_2).build()
+  val bucketName = config.getString("randomLogGenerator.s3BucketName")
+  val key = "log"
+
+  val logFuture = Future {
+    LogMsgSimulator(init(RandomStringGenerator((Parameters.minStringLength, Parameters.maxStringLength), Parameters.randomSeed)), Parameters.maxCount)
+  }
+
+  Try(Await.result(logFuture, Parameters.runDurationInMinutes)) match {
+    case Success(value) => {
+      logger.info(s"Log data generation has completed after generating ${Parameters.maxCount} records.")
+
+      // Now I upload the log file to S3 and I delete the local file to avoid wasting resources
+
+/*      val getRequest: GetObjectRequest = new GetObjectRequest(bucketName, key)
+      val S3Object: S3Object = s3Client.getObject(getRequest)
+      val objectData: java.io.InputStream = S3Object.getObjectContent() // existing log file in S3*/
+      
+      val filePath = "log/log"
+      val file = new java.io.File(filePath)
+
+      val request: PutObjectRequest = new PutObjectRequest(bucketName, key, file)
+      val metadata: ObjectMetadata = new ObjectMetadata()
+      metadata.setContentType("plain/text")
+      request.setMetadata(metadata)
+      s3Client.putObject(request)
+      file.delete()
+    }
+    case Failure(exception) => logger.info(s"Log data generation has completed within the allocated time, ${Parameters.runDurationInMinutes}")
+  }
+
