5b6cdfe0165778e6e684edceb7888c9b9641abdb
CosimoSguanci <cosimo.sguanci@mail.polimi.it>
2021-11-01 16:07:09 -0500

    Fixes in LogFileGenerator

--- LogFileGenerator/src/main/scala/GenerateLogData.scala
+++ LogFileGenerator/src/main/scala/GenerateLogData.scala
@@ -11,7 +11,7 @@ import Generation.{LogMsgSimulator, RandomStringGenerator}
 import HelperUtils.{CreateLogger, ObtainConfigReference, Parameters}
 import com.amazonaws.auth.{AWSStaticCredentialsProvider, BasicAWSCredentials}
 import com.amazonaws.regions.Regions
-import com.amazonaws.services.s3.model.{GetObjectRequest, ObjectMetadata, PutObjectRequest, S3Object}
+import com.amazonaws.services.s3.model.{AmazonS3Exception, GetObjectRequest, ObjectMetadata, PutObjectRequest, S3Object}
 
 import collection.JavaConverters.*
 import scala.concurrent.{Await, Future, duration}
@@ -20,6 +20,7 @@ import scala.concurrent.duration.Duration
 import scala.io.Source
 import scala.util.{Failure, Success, Try}
 import com.amazonaws.services.s3.{AmazonS3, AmazonS3ClientBuilder}
+import org.apache.commons.io.FileUtils
 
 object GenerateLogData:
   val logger = CreateLogger(classOf[GenerateLogData.type])
@@ -43,7 +44,7 @@ object GenerateLogData:
   val awsCredentials: BasicAWSCredentials = new BasicAWSCredentials(config.getString("randomLogGenerator.awsAccessKey"), config.getString("randomLogGenerator.awsSecretKey"))
   val s3Client: AmazonS3 = AmazonS3ClientBuilder.standard().withCredentials(new AWSStaticCredentialsProvider(awsCredentials)).withRegion(Regions.US_EAST_2).build()
   val bucketName = config.getString("randomLogGenerator.s3BucketName")
-  val key = "log"
+  val key = config.getString("randomLogGenerator.s3ObjectKey")
 
   val logFuture = Future {
     LogMsgSimulator(init(RandomStringGenerator((Parameters.minStringLength, Parameters.maxStringLength), Parameters.randomSeed)), Parameters.maxCount)
@@ -55,20 +56,68 @@ object GenerateLogData:
 
       // Now I upload the log file to S3 and I delete the local file to avoid wasting resources
 
-/*      val getRequest: GetObjectRequest = new GetObjectRequest(bucketName, key)
-      val S3Object: S3Object = s3Client.getObject(getRequest)
-      val objectData: java.io.InputStream = S3Object.getObjectContent() // existing log file in S3*/
-      
-      val filePath = "log/log"
-      val file = new java.io.File(filePath)
-
-      val request: PutObjectRequest = new PutObjectRequest(bucketName, key, file)
-      val metadata: ObjectMetadata = new ObjectMetadata()
-      metadata.setContentType("plain/text")
-      request.setMetadata(metadata)
-      s3Client.putObject(request)
-      file.delete()
+      try {
+        val getRequest: GetObjectRequest = new GetObjectRequest(bucketName, key)
+        val S3Object: S3Object = s3Client.getObject(getRequest)
+        val objectData: java.io.InputStream = S3Object.getObjectContent() // existing log file in S3
+        addLogToS3(objectData)
+      } catch {
+        case ex: AmazonS3Exception => {
+          // This means that there are no log files in S3 bucket
+          initLogS3()
+        }
+      }
     }
     case Failure(exception) => logger.info(s"Log data generation has completed within the allocated time, ${Parameters.runDurationInMinutes}")
+  }
+
+  /**
+   * Called to upload a log file to AWS S3 for the first time (no object with the key "log" was found)
+   */
+  def initLogS3() = {
+    val dirName = "log"
+    val dir = new java.io.File(dirName)
+    val fileName = if(dir.exists && dir.isDirectory) dir.listFiles.filter(_.isFile).toList(0).getName else null
+    val filePath = s"$dirName/$fileName"
+    val newLogFile = new java.io.File(filePath)
+
+    val request: PutObjectRequest = new PutObjectRequest(bucketName, key, newLogFile)
+    val metadata: ObjectMetadata = new ObjectMetadata()
+    metadata.setContentType("plain/text")
+    request.setMetadata(metadata)
+    s3Client.putObject(request)
+
+    newLogFile.delete()
+  }
+
+  /**
+   * Called to add logs to the already exisiting log object on AWS S3
+   * @param previousObjectData the InputStream related to the logs already stored in S3. Its content will be merged with the new logs, and the new log object will be uploaded
+   */
+  def addLogToS3(previousObjectData: java.io.InputStream) = {
+    val logFile = new java.io.File("log/log.tmp")
+    FileUtils.copyInputStreamToFile(previousObjectData, logFile)
+
+    val dirName = "log"
+    val dir = new java.io.File(dirName)
+    val fileName = if(dir.exists && dir.isDirectory) dir.listFiles.filter(_.isFile).toList(0).getName else null
+    val filePath = s"$dirName/$fileName"
+    val newLogFile = new java.io.File(filePath)
+
+    // we need to merge logFile and newLogFile
+    val oldContent: String = FileUtils.readFileToString(logFile, "UTF-8")
+    val newContent: String = FileUtils.readFileToString(newLogFile, "UTF-8")
+
+    FileUtils.write(logFile, oldContent, "UTF-8")
+    FileUtils.write(logFile, newContent, "UTF-8", true)
+
+    val request: PutObjectRequest = new PutObjectRequest(bucketName, key, logFile)
+    val metadata: ObjectMetadata = new ObjectMetadata()
+    metadata.setContentType("plain/text")
+    request.setMetadata(metadata)
+    s3Client.putObject(request)
+
+    logFile.delete()
+    newLogFile.delete()
   }
 
